24251b9d6ff1d61dbe6da3de374defee54bfc381
ines <akez.ines@gmail.com>
2025-01-06 16:32:43 +0100

    feat(auth): Implement authentication controller - Login endpoint - Token management

---
+++ src/main/java/com/ines/auth/AuthenticationService.java
@@ -0,0 +1,40 @@
+package com.ines.auth;
+
+import com.ines.dto.CustomerDTO;
+import com.ines.jwt.JWTUtil;
+import com.ines.mapper.CustomerDTOMapper;
+import com.ines.model.Customer;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthenticationService {
+
+    private final AuthenticationManager authenticationManager;
+    private final CustomerDTOMapper customerDTOMapper;
+    private final JWTUtil jwtUtil;
+
+    public AuthenticationService(AuthenticationManager authenticationManager,
+                                 CustomerDTOMapper customerDTOMapper,
+                                 JWTUtil jwtUtil) {
+        this.authenticationManager = authenticationManager;
+        this.customerDTOMapper = customerDTOMapper;
+        this.jwtUtil = jwtUtil;
+    }
+
+    public AuthenticationResponse login(AuthenticationRequest request) {
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.username(),
+                        request.password()
+                )
+        );
+        Customer principal = (Customer) authentication.getPrincipal();
+        CustomerDTO customerDTO = customerDTOMapper.apply(principal);
+        String token = jwtUtil.issueToken(customerDTO.username(), customerDTO.roles());
+        return new AuthenticationResponse(token, customerDTO);
+    }
+
+}
